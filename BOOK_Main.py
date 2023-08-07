8a3132cb21e1b1c7a0a72eca03665d7044e181c0
Alex Temple <TempleAlexander95@gmail.com>
2023-08-07 19:51:04 -0400

    Adding intial classes and MAIN program

---
+++ BOOK_Main.py
@@ -0,0 +1,9 @@
+# Leaving room for the imports and any other comments
+#Created for a Rutgers University MBS Final
+
+
+
+class BOOK():
+
+    def __init__(self, ISBN, Title, Author, DatePublished, ) -> None:
+        pass
