145f793084059577ab06c79d03076fa84e1d3799
Alex Temple <alex.temple@noblis.org>
2023-08-08 10:45:58 -0400

    Added __str__ and used functions to set book parameters for firstbook print

--- BOOK_Main.py
+++ BOOK_Main.py
@@ -1,26 +1,26 @@
 # Leaving room for the imports and any other comments
 #Created for a Rutgers University MBS Final
 
-
+import csv
 
 
 # Remember!  == for comparison , = for assigning
 class book():
 
     def __init__(self, Title, Author, ISBNnum=None, datePublished=None, readingStatus=None, bookFormat=None, location=None):
-        self.ISBN = ISBNnum
-        self.Title = Title
-        self.Author = Author
+        self.ISBNnum = ISBNnum
+        self.Title = "Title"
+        self.Author = "Author"
         self.datePublished = datePublished
         self.readingStatus = readingStatus
         self.bookFormat = bookFormat
         self.location = location
 
     def getISBN(self): # Getting the ISBN // OPTIONAL FOR USER
-        return self.ISBN
+        return self.ISBNnum
     
     def setISBN(self, ISBNnum): # Setting the ISBN // OPTIONAL FOR USER
-        self.ISBN = ISBNnum
+        self.ISBNnum = ISBNnum
     
     def getTitle(self): # Getting Title
         return self.Title
@@ -58,6 +58,11 @@ class book():
     def setlocation(self, location): # Setting location // OPTIONAL FOR USER
         self.location = location
 
+    def __str__(self): # self printer
+            return f"Title: {self.Title} \nAuthor: {self.Author} \nISBN: {self.ISBNnum} \nLocation: {self.location}\nBook Format: {self.bookFormat}\nReading Status: {self.readingStatus}\nDate Published: {self.datePublished}\n"
 
 firstbook = book("The Adventures of Huckleberry Finn","Mark Twain")
-print (firstbook())
+firstbook(book.setAuthor = "Mark Twain")
+firstbook(book.setTitle = "The Adventures of Huckleberry Finn")
+
+print (firstbook)
